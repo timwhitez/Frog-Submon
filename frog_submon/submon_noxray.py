f31aa9e441a2b6c8e8dc3cf82439bd64a4fb3149
TimWhite <36320909+timwhitez@users.noreply.github.com>
2020-09-07 12:37:56 +0800

    Update submon_noxray.py

--- frog_submon/submon_noxray.py
+++ frog_submon/submon_noxray.py
@@ -141,7 +141,7 @@ def ksub():
 
 #ksub验证模式
 def ksubverify(filename):
-	cmd = ["./ksub/"+ksubname,"-f", filename, "-verify", "-silent", "-b", "400k"]
+	cmd = ["./ksub/"+ksubname,"-f", filename, "-verify", "-skip-wild", "-silent", "-b", "400k"]
 	print(cmd)
 	try:
 		output = subprocess.check_output(cmd)
